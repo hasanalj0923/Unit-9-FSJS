a16cc877ff5727d6153ef9c0c0e1e789f6ee5991
hasanalj0923 <hasanhnaljarrah@gmail.com>
2025-08-13 18:22:25 +0100

    Add files via upload

---
+++ migrations/20250813000200-create-course.js
@@ -0,0 +1,53 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Courses', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            title: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            description: {
+                type: Sequelize.TEXT,
+                allowNull: false
+            },
+            estimatedTime: {
+                type: Sequelize.STRING,
+                allowNull: true
+            },
+            materialsNeeded: {
+                type: Sequelize.STRING,
+                allowNull: true
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'Users',
+                    key: 'id'
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE'
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.fn('NOW')
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.fn('NOW')
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Courses');
+    }
+};
